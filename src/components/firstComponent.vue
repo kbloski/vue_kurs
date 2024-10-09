b21b812dff39943b4c85e2102a523f9f747e6859
Kamil Błoński <kblonski02@gmail.com>
2024-10-09 15:17:40 +0200

    Firs changes in project

---
+++ src/components/firstComponent.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div>
+        Hello from my first vue Component
+    </div>
+</template>
+
+
+<style scoped>
+    div {
+        color: green;
+    }
+</style>
