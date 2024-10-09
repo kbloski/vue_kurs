d9d4557ba673599f8c52c67f60f72c625b82fad1
Kamil Błoński <kblonski02@gmail.com>
2024-10-09 18:40:39 +0200

    event form

--- 02_events/app.js
+++ 02_events/app.js
@@ -2,15 +2,20 @@ const vueApp = Vue.createApp({
     data(){
         return {
             counter: 0,
-            description: 'example description'
+            name: 'Default name'
         }
     },
     methods: {
         clearCounter(){
             this.counter = 0
         },
-        setDescription( text ){
-            this.description =  text
+        setName( event ){
+            this.name = event.target.value
+        },
+        submitForm(event){
+            event.preventDefault();
+            alert("Was submit");
+            console.log('Submitted')
         }
     }
 }
