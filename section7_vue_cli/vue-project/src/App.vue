b67f57d30cfa90c27d4b50444103b255b79fbd1a
Kamil Błoński <kblonski02@gmail.com>
2024-10-14 19:11:05 +0200

    Finish section7

--- section7_vue_cli/vue-project/src/App.vue
+++ section7_vue_cli/vue-project/src/App.vue
@@ -1,31 +1,97 @@
 <template>
-    <section>
-        <h2>My Friends</h2>
-        <ul v-for="friend in friends" v-bind:key="friend.id">
-            <li>{{ friend.id }}</li>
-        </ul>
-    </section>
+  <section>
+    <header>My Friends</header>
+    <ul v-for="friend in friends" v-bind:key="friend.id">
+      <li>{{ friend.id }}</li>
+    </ul>
+  </section>
 </template>
 
 <script lang="ts">
-    export default {
-    data() {
-        return {
-        friends: [
-            {
-            id: "manuel",
-            name: "Manuel Lorenz ",
-            phone: "01231232131",
-            email: "manuel@local.host",
-            },
-            {
-            id: "julie",
-            name: "Julie Julie ",
-            phone: "01231232131",
-            email: "Julie@local.host",
-            },
-        ],
-        };
-    },
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz ",
+          phone: "01231232131",
+          email: "manuel@local.host",
+        },
+        {
+          id: "julie",
+          name: "Julie Julie ",
+          phone: "01231232131",
+          email: "Julie@local.host",
+        },
+      ],
     };
+  },
+};
 </script>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
