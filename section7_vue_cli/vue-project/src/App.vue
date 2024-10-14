0f5fac91d2902d09353f9385e0f5ea54b0f810db
Kamil Błoński <kblonski02@gmail.com>
2024-10-14 18:57:39 +0200

    Start with vue components

--- section7_vue_cli/vue-project/src/App.vue
+++ section7_vue_cli/vue-project/src/App.vue
@@ -1,41 +1,31 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+    <section>
+        <h2>My Friends</h2>
+        <ul v-for="friend in friends" v-bind:key="friend.id">
+            <li>{{ friend.id }}</li>
+        </ul>
+    </section>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<script lang="ts">
+    export default {
+    data() {
+        return {
+        friends: [
+            {
+            id: "manuel",
+            name: "Manuel Lorenz ",
+            phone: "01231232131",
+            email: "manuel@local.host",
+            },
+            {
+            id: "julie",
+            name: "Julie Julie ",
+            phone: "01231232131",
+            email: "Julie@local.host",
+            },
+        ],
+        };
+    },
+    };
+</script>
