c458b35fa26a7d18e8e6a27f1eaafc6411b24516
Kamil Błoński <kblonski02@gmail.com>
2024-10-14 16:38:08 +0200

    behind the scenes

--- section5_vue_behindTheScenes/app.js
+++ section5_vue_behindTheScenes/app.js
@@ -10,7 +10,35 @@ const app = Vue.createApp({
         setText(){
             this.message = this.$refs.userText.value;
         }
+    },
+    beforeCreate() {
+        console.log( 'beforeCreate()')
+    },
+    created(){
+        console.log('created()')
+    },
+    beforeMount(){
+        console.log('bveforeMount()')
+    },
+    mounted(){
+        console.log('mounted()')
+    },
+    beforeUpdate(){
+        console.log('beforeUpdate()');
+    },
+    updated(){
+        console.log('updated()')
+    },
+    beforeUnmount(){
+        console.log('beforeUnmount()')
+    },
+    unmounted(){
+        console.log('unmounted()')
     }
 });
 
 app.mount('#app');
+
+setTimeout( ()=>{
+    app.unmount();
+}, 3000)
