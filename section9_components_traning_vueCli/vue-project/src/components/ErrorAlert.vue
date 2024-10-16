8b4018445d397881016e8bf49f7418e5854c5dc4
Kamil Błoński <kblonski02@gmail.com>
2024-10-16 20:39:42 +0200

    alert dialog

---
+++ section9_components_traning_vueCli/vue-project/src/components/ErrorAlert.vue
@@ -0,0 +1,18 @@
+<template>
+    <dialog open="true">
+        <slot></slot>
+    </dialog>
+</template>
+
+<style scoped>
+    dialog {
+        display: block;
+        position: fixed;
+        top: 20vh;
+        left: 30%;
+        width: 40%;
+        background-color: white;
+        box-shadow: 0 2px 8px rgba(0,0,0,0.3);
+        padding: 1rem;
+    }
+</style>
--- section9_components_traning_vueCli/vue-project/src/components/ManageGoals.vue
+++ section9_components_traning_vueCli/vue-project/src/components/ManageGoals.vue
@@ -1,6 +1,39 @@
 <template>
-    <div>
-         <h2>Manage Goals</h2>
-         <input type="text" />
-    </div>
+         <div>
+            <h2>Manage Goals</h2>
+            <input type="text" ref="goal"/>
+            <button @click="setGoals">Set goal</button>
+            <ErrorAlert v-if="inputIsInvalid">
+                    <h2>Input is Invalid!</h2>
+                    <p>Please enter at least a few character</p>
+                    <button @click="confirmError">Ok</button>
+            </ErrorAlert>
+         </div>
 </template>
+
+<script lang="ts">
+import ErrorAlert from './ErrorAlert.vue';
+
+export default {
+    components: {
+        ErrorAlert
+    },
+    data() {
+        return {
+            inputIsInvalid: false
+        }
+    },
+    methods: {
+        setGoals(){
+            const enteredValue = this.$refs.goal as HTMLInputElement;
+            if(enteredValue.value === ''){
+                this.inputIsInvalid = true;
+            } 
+        },
+        confirmError(){
+            this.inputIsInvalid= false;
+        }
+
+    }
+}
+</script>
