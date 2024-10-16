71f85723143eba81eebbb9a6524e8021afeada0d
Kamil Błoński <kblonski02@gmail.com>
2024-10-16 21:53:11 +0200

    first step in new project

--- section10_course_project_theLearningResourcesApp/vue-project/src/App.vue
+++ section10_course_project_theLearningResourcesApp/vue-project/src/App.vue
@@ -1,3 +1,39 @@
 <template>
-  <h1>Init aplication</h1>
+  <ul>
+    <learning-resource 
+      v-for="res in storeResources" 
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
 </template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResourceItem.vue';
+
+export default {
+  components: {
+    LearningResource
+  },
+  data(){
+    return {
+      storeResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The oficial Vue.js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org'
+        },
+      ]
+    }
+  }
+}
+</script>
