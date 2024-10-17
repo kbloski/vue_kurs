e222678aac6b21c0099d938f9052324bce601146
Kamil Błoński <kblonski02@gmail.com>
2024-10-17 10:22:46 +0200

    add style for nav

--- section10_course_project_theLearningResourcesApp/vue-project/src/components/learning-resources/TheResources.vue
+++ section10_course_project_theLearningResourcesApp/vue-project/src/components/learning-resources/TheResources.vue
@@ -1,31 +1,48 @@
 <template>
+  <div>
     <base-card>
-        <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
-        <base-button @click="setSelectedTab('add-resource')">Add Resource</base-button>
-        <component  component :is="selectedTab" :resources="resources"></component>
+      <base-button
+        @click="setSelectedTab('stored-resources')"
+        :mode="storedResButtonMode"
+        >Stored Resources</base-button
+      >
+      <base-button
+        @click="setSelectedTab('add-resource')"
+        :mode="addResButtonMode"
+        >Add Resource</base-button
+      >
     </base-card>
+    <component component :is="selectedTab" :resources="resources"></component>
+  </div>
 </template>
 
 <script>
-import StoredResources from './StoredResources.vue';
-import AddResource from './AddResource.vue';
+import StoredResources from "./StoredResources.vue";
+import AddResource from "./AddResource.vue";
 
 export default {
-    components: {
-        StoredResources,
-        AddResource
+  components: {
+    StoredResources,
+    AddResource,
+  },
+  inject: ["resources"],
+  data() {
+    return {
+      selectedTab: "stored-resources",
+    };
+  },
+  computed: {
+    storedResButtonMode(){
+        return this.selectedTab === 'stored-resources' ? null : 'flat'
     },
-    inject: ['resources'],
-    data(){
-        return {
-            selectedTab: 'stored-resources'
-        }
-    },
-    methods: {
-        setSelectedTab(tab){
-            this.selectedTab = tab;
-            console.log( this.storeResources)
-        }
+    addResButtonMode(){
+        return this.selectedTab === 'add-resource' ? null : 'flat'
     }
-}
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+};
 </script>
