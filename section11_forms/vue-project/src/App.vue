017a632fdf08266b572a4d206812932f3d61f640
Kamil Błoński <kblonski02@gmail.com>
2024-10-17 14:19:11 +0200

    build function rating v-model

--- section11_forms/vue-project/src/App.vue
+++ section11_forms/vue-project/src/App.vue
@@ -1,3 +1,13 @@
 <template>
-  
+  <the-form></the-form>
 </template>
+
+<script>
+import TheForm from './components/layout/TheForm.vue'
+
+export default {
+  components: {
+    TheForm
+  }
+}
+</script>
---
+++ section11_forms/vue-project/src/components/layout/RatingControle.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul>
+    <li :class="{ active: activeOption === 'poor' }">
+      <button @click="activate('poor')">Poor</button>
+    </li>
+    <li :class="{ active: activeOption === 'averange' }">
+      <button @click="activate('averange')">Average</button>
+    </li>
+    <li :class="{ active: activeOption === 'great' }">
+      <button @click="activate('great')">Great</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["modelValue"],
+  emits: ["update:modelValue"],
+  data() {
+    return {
+      activeOption: this.modelValue,
+    };
+  },
+  methods: {
+    activate(option) {
+      this.activeOption = option;
+      this.$emit("update:modelValue", option);
+    },
+  },
+};
+</script>
+
+<style>
+.active {
+  border-color: green;
+}
+
+.active button {
+  border-color: green;
+}
+</style>
