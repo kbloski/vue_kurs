ca62a581311b4d04c1fc222e8a21623db0c82557
Kamil Błoński <kblonski02@gmail.com>
2024-10-17 10:33:15 +0200

    Add form resources

--- section10_course_project_theLearningResourcesApp/vue-project/src/components/learning-resources/AddResource.vue
+++ section10_course_project_theLearningResourcesApp/vue-project/src/components/learning-resources/AddResource.vue
@@ -1,3 +1,73 @@
 <template>
-    <h2>Add resource</h2>
+  <div>
+    <base-card>
+      <form>
+        <div class="form-control">
+          <label for="title">Title</label>
+          <input id="title" name="title" type="text" ref="titleInput" />
+        </div>
+        <div class="form-control">
+          <label for="description">Description</label>
+          <textarea
+            id="description"
+            name="description"
+            rows="3"
+            ref="descriptionInput"
+          ></textarea>
+        </div>
+        <div class="form-control">
+          <label for="link">Link</label>
+          <input id="link" name="link" type="url" ref="linkInput" />
+        </div>
+        <div>
+          <base-button type="submit" @click="submitData">Add resource</base-button>
+        </div>
+      </form>
+    </base-card>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    submitData(event){
+        event.preventDefault();
+
+        const enteredTitle = this.$refs.titleInput.value;
+        const enteredDescription = this.$refs.descriptionInput.value;
+        const enteredLink = this.$refs.link.value;
+        
+        console.log( this.$refs)
+
+    }
+  }
+};
+</script>
+
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
