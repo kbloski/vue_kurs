a288b6ac88599266e6fb881bdf2aff20e8a73003
Kamil Błoński <kblonski02@gmail.com>
2024-10-21 20:10:51 +0200

    enter to transition component with classes

--- section14_animationsTransitions/vue-project/src/App.vue
+++ section14_animationsTransitions/vue-project/src/App.vue
@@ -4,8 +4,13 @@
       <div class="block" :class="{animate: animatedBlock}"></div>
       <button @click="animateBlock">Animate</button>
     </div>
+    <div class="container">
+      <transition>
+        <p v-if="paraIsVisible">This is only sometimes visible...</p>
+      </transition>
+      <button @click="toggleParagraph">Toggle Paraph</button>
+    </div>
     <base-modal @close="hideDialog" v-if="dialogIsVisible">
-      <p>This is a test dialog!</p>
       <button @click="hideDialog">Close it!</button>
     </base-modal>
     <div class="container">
@@ -18,6 +23,7 @@
 export default {
   data() {
     return { 
+      paraIsVisible: false,
       animatedBlock: false,
       dialogIsVisible: false,
      };
@@ -31,6 +37,9 @@ export default {
     },
     animateBlock(){
       this.animatedBlock = !this.animatedBlock
+    },
+    toggleParagraph(){
+      this.paraIsVisible = !this.paraIsVisible
     }
   },
 };
@@ -82,6 +91,34 @@ button:active {
 
 .animate {
   animation: fade-rotate 1s ease-out forwards;
+}
+
+
+.v-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.v-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.v-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.v-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+.v-leave-active {
+  transition: all 0.3s ease-in;
+}
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+
 }
 
 @keyframes fade-rotate {
