e6ab8d5f5238fcbcbaa2b3b0cc11653ee84164c6
Kamil Błoński <kblonski02@gmail.com>
2024-10-22 21:32:38 +0200

    Description route-transition

--- section14_animationsTransitions/vue-project/src/pages/AllUsers.vue
+++ section14_animationsTransitions/vue-project/src/pages/AllUsers.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="container">
-        <h2>All Users</h2>
-        <router-link to="/goals">Course Goals</router-link>
+        <h2>All Users Page</h2>
     </div>
 </template>
--- section14_animationsTransitions/vue-project/src/pages/CourseGoals.vue
+++ section14_animationsTransitions/vue-project/src/pages/CourseGoals.vue
@@ -1,7 +1,5 @@
 <template>
     <div class="container">
-        <h2>Course Goals</h2>
-        <router-link to="/">Users</router-link>
-
+        <h2>Course Goals Page</h2>
     </div>
 </template>
