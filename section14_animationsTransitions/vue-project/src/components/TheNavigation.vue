8a28bde760c51fd316f83c46cd9828f13228e32f
Kamil Błoński <kblonski02@gmail.com>
2024-10-22 21:51:12 +0200

    Update nav

--- section14_animationsTransitions/vue-project/src/components/TheNavigation.vue
+++ section14_animationsTransitions/vue-project/src/components/TheNavigation.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
-        <router-link to="/"><button>Transition Page</button></router-link>
-        <router-link to="/users"><button>Users Page</button></router-link>
-        <router-link to="/goals"><button>Goals Page</button></router-link>
+        <router-link to="/"><button>Transition Route</button></router-link>
+        <router-link to="/users"><button>Users Route</button></router-link>
+        <router-link to="/goals"><button>Goals Route</button></router-link>
     </div>
 </template>
