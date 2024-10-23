54da4217aa082f8676f279d9926af23e661f940c
Kamil Błoński <kblonski02@gmail.com>
2024-10-23 19:36:32 +0200

    Add content to page users

--- section14_animationsTransitions/vue-project/src/pages/AllUsers.vue
+++ section14_animationsTransitions/vue-project/src/pages/AllUsers.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
         <h2>All Users Route</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, neque?</p>
     </div>
 </template>
