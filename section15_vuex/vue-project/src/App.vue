7402c56f4f467ed59ca6f1f8a9e7cc8068ef155a
Kamil Błoński <kblonski02@gmail.com>
2024-10-23 19:47:48 +0200

    Prepare Example Component

--- section15_vuex/vue-project/src/App.vue
+++ section15_vuex/vue-project/src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
     <h1>Init vuex project</h1>
+    <BaseCmp></BaseCmp>
   </div>
 </template>
+
 <script>
+import BaseCmp from './components/BaseCmp.vue'
+
+export default {
+  components: {
+    BaseCmp
+  },
+  data(){
+
+  }
+}
 </script>
---
+++ section15_vuex/vue-project/src/components/BaseCmp.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h2>Example component</h2>
+    </div>
+</template>
