9ff57fc773615b336f3ae5375c08098c0e23f6e8
Kamil Błoński <kblonski02@gmail.com>
2024-10-23 19:36:52 +0200

    Add content to page goals

--- section14_animationsTransitions/vue-project/src/pages/CourseGoals.vue
+++ section14_animationsTransitions/vue-project/src/pages/CourseGoals.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="container">
         <h2>Course Goals Route</h2>
+        <ul>
+            <li>Goal1</li>
+            <li>Goal2</li>
+            <li>Goal3</li>
+        </ul>
     </div>
 </template>
