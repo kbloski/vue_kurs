95864076f95e98ff3cde70aa6879ae7d82566382
Kamil Błoński <kblonski02@gmail.com>
2024-10-25 13:05:39 +0200

    Prepare routes

--- section16_mainProject_FindACoach/vue-project/src/main.js
+++ section16_mainProject_FindACoach/vue-project/src/main.js
@@ -1,8 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router.js';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router.js";
+import store from "./store/index.js";
 
-const appVue = createApp(App)
-appVue.use( router );
+const appVue = createApp(App);
 
-appVue.mount('#app')
+appVue.use(router);
+appVue.use(store);
+
+appVue.mount("#app");
--- section16_mainProject_FindACoach/vue-project/src/router.js
+++ section16_mainProject_FindACoach/vue-project/src/router.js
@@ -1,10 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "./pages/HomePage.vue";
-import store from "./store/index.js";
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: [{ path: "/", component: HomePage }],
+    routes: [
+        { path: "/", redirect: '/coaches' },
+        { path: "/coaches", component: null, children:[
+            { path: "/contact", component: null }, // /coaches/c1/contact
+        ] },
+        { path: "/coaches/:id", component: null },
+        { path: "/register", component: null },
+        { path: "/requests", component: null },
+        { path: "/:catchAll(.*)", component: null },
+    ],
 });
 
 export default router;
