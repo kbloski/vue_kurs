85f8597a9d609f7221e11b788f3fd2c42438758a
Kamil Błoński <kblonski02@gmail.com>
2024-10-26 21:10:41 +0200

    clean code[A

--- section17_vueAuthentication/vue-project/src/components/layout/TheNavigation.vue
+++ section17_vueAuthentication/vue-project/src/components/layout/TheNavigation.vue
@@ -2,32 +2,35 @@
     <base-card>
         <h2>Navigation</h2>
         <ul>
-            <li v-if="!isLoggedIn"><base-button link to="/login">Login Page</base-button></li>
+            <li v-if="!isLoggedIn">
+                <base-button link to="/login">Login Page</base-button>
+            </li>
 
-            <li v-if="isLoggedIn"><base-button link to="/dashboard">Dashboard</base-button></li>
+            <li v-if="isLoggedIn">
+                <base-button link to="/dashboard">Dashboard</base-button>
+            </li>
             <li v-if="isLoggedIn"><base-button @click="logout">Logout</base-button></li>
         </ul>
     </base-card>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
 
 export default {
     computed: {
-        ...mapGetters('auth', ['isLoggedIn']),
+        ...mapGetters("auth", ["isLoggedIn"]),
     },
     methods: {
-        logout(){
-            this.$store.dispatch('auth/logout')
-            this.$router.push('/login')
-        }
-    }
-}
+        logout() {
+            this.$store.dispatch("auth/logout");
+            this.$router.push("/login");
+        },
+    },
+};
 </script>
 
 <style scoped>
-
 ul {
     list-style: none;
     margin: 0;
