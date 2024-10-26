c5bc7d0bd62678205236c32ea06f33ac5a563906
Kamil Błoński <kblonski02@gmail.com>
2024-10-26 21:25:16 +0200

    Add guards to routes

--- section17_vueAuthentication/vue-project/src/router.js
+++ section17_vueAuthentication/vue-project/src/router.js
@@ -1,15 +1,31 @@
 import { createRouter, createWebHistory } from "vue-router";
 import UserAuth from "./pages/UserAuth.vue";
 import Dashboard from "./pages/Dashboard.vue";
+import store from "./store/index.js";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', redirect: '/login'},
-        { path: '/login', component: UserAuth},
-        { path: '/dashboard', component: Dashboard},
+        { path: "/", redirect: "/login" },
+        {
+            path: "/login",
+            component: UserAuth,
+            beforeEnter(to, from, next) {
+                const isLogged = store.getters["auth/isLoggedIn"];
+                if (isLogged) {
+                    return next("/dashboard");
+                }
+                next();
+            },
+        },
+        { path: "/dashboard", component: Dashboard },
+    ],
+});
 
-    ]
-})
+router.beforeEach((to, from, next) => {
+    const isLogged = store.getters["auth/isLoggedIn"];
+    if (to.path === "/dashboard" && !isLogged) next("/login");
+    next();
+});
 
 export default router;
