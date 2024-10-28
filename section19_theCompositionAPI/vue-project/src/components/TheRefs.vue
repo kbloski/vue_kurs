5111f19c5645d154d745f295566c2d659b08b845
Kamil Błoński <kblonski02@gmail.com>
2024-10-28 21:21:07 +0100

    Ref

--- section19_theCompositionAPI/vue-project/src/components/TheRefs.vue
+++ section19_theCompositionAPI/vue-project/src/components/TheRefs.vue
@@ -1,3 +1,32 @@
 <template>
-    <h1> The refs component</h1>
+    <div>
+        <h1>The refs component</h1>
+        <h2>{{ name }} {{ surname }}</h2>
+    </div>
 </template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+    setup() {
+        const uName = ref("Kamil"); // utworzenie wartości reaktywnej
+        let uSurname = 'Błoński';
+
+        setTimeout( ()=>{
+            uName.value = 'Milblo'
+            uSurname = 'This not be working'
+        }, 1000)
+
+        return {
+            name: uName,
+            surname: uSurname
+        };
+    },
+    // data(){
+    //     return {
+    //         userName: 'Kamil'
+    //     }
+    // }
+};
+</script>
