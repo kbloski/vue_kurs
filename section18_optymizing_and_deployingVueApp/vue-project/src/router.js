fa0653b84a7d8f4713bd45b0693ff15b4012d8ec
Kamil Błoński <kblonski02@gmail.com>
2024-10-28 21:07:10 +0100

    init section 19

--- section18_optymizing_and_deployingVueApp/vue-project/src/router.js
+++ section18_optymizing_and_deployingVueApp/vue-project/src/router.js
@@ -1,9 +1,10 @@
 import { defineAsyncComponent } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
-import UserAuth from "./pages/UserAuth.vue";
+// import UserAuth from "./pages/UserAuth.vue";
 // import Dashboard from "./pages/Dashboard.vue";
 import store from "./store/index.js";
 
+const UserAuth = defineAsyncComponent(() => import("./pages/UserAuth.vue"));
 const Dashboard = defineAsyncComponent(() => import("./pages/Dashboard.vue"));
 
 const router = createRouter({
