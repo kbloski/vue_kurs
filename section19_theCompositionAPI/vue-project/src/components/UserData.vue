31fba021bfff4949a02e0d98596868f9a82251c8
Kamil Błoński <kblonski02@gmail.com>
2024-10-28 23:36:16 +0100

    Emit data

---
+++ section19_theCompositionAPI/vue-project/src/components/UserData.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h2>Component with props</h2>
+        <h3>fullname: {{  fullname  }}</h3>
+        <h4>firstname: {{ firstName}}</h4>
+        <h4>lastname: {{ lastName}}</h4>
+        <h4>age:{{  age }} </h4>
+    </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+    props: ['firstName', 'lastName', 'age'],
+    setup(props, context){
+        const fullname = computed(()=>{
+            return props.firstName + ' ' + props.lastName
+        })
+
+        // console.log( context )
+        context.emit('save-data', 1);
+
+        return {
+            fullname,
+        }
+    }
+}
+</script>
+
+<style scoped>
+div {
+    border: 2px solid black;
+    border-radius: 5px;
+    padding: 10px;
+}
+</style>
--- section19_theCompositionAPI/vue-project/src/pages/TheProps.vue
+++ section19_theCompositionAPI/vue-project/src/pages/TheProps.vue
@@ -1,5 +1,42 @@
 <template>
     <div>
-        <h1>Props</h1>
+        <h1>Props & compositionAPI</h1>
+        <user-data 
+            :first-name="firstname" 
+            :last-name="lastname" 
+            :age="age"
+            @save-data="showEmitData"
+        ></user-data>
     </div>
 </template>
+
+<script>
+import { reactive, ref, toRefs } from "vue";
+import UserData from "../components/UserData.vue";
+
+export default {
+    components: {
+        UserData,
+    },
+    setup() {
+        const userdata = reactive({
+            name: "Kamil",
+            surname: "Błoński",
+        });
+        const age = ref(20);
+
+
+        function showEmitData( data ){
+            console.log('emit save-data: ' + data)
+        }
+
+        const user = toRefs(userdata);
+        return {
+            firstname: user.name,
+            lastname: user.surname,
+            age,
+            showEmitData
+        };
+    },
+};
+</script>
