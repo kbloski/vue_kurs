4463f1972e9481765144967b857867dea76d3405
Kamil Błoński <kblonski02@gmail.com>
2024-10-29 22:38:13 +0100

    Refactor App.vue to compositionAPI

--- section19_theCompositionAPI/trainViewSetup/src/App.vue
+++ section19_theCompositionAPI/trainViewSetup/src/App.vue
@@ -10,23 +10,27 @@ import USER_DATA from './dummy-data.js';
 
 import UserList from './components/users/UserList.vue';
 import ProjectsList from './components/projects/ProjectsList.vue';
+import { ref, reactive } from 'vue';
 
 export default {
   components: {
     UserList,
     ProjectsList,
   },
-  data() {
+  setup(){
+    const selectedUser = ref(null);
+    const activeUsers = reactive( USER_DATA );
+
+    function selectUser(uid) {
+      selectedUser.value = activeUsers.find((usr) => usr.id === uid);
+    }
+
     return {
-      selectedUser: null,
-      activeUsers: USER_DATA,
-    };
-  },
-  methods: {
-    selectUser(uid) {
-      this.selectedUser = this.activeUsers.find((usr) => usr.id === uid);
-    },
-  },
+      selectedUser,
+      activeUsers,
+      selectUser
+    } 
+  }
 };
 </script>
 
